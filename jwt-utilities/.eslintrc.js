eb9b2a46073c9cab3cf0cd002d13bc9147829c9b
cetherdev <robert.hct@gmail.com>
2021-02-27 13:43:49 -0600

    eslint

---
+++ jwt-utilities/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    strict: 0,
+    indent: ['error', 2],
+    semi: ['error', 'single']
+  }
+}

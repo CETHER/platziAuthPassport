77de87d892a5362b1389bb8ee6742243e427e2f6
cetherdev <robert.hct@gmail.com>
2021-03-01 17:14:48 -0600

    eslint

--- movies-api/utils/schemas/users.js
+++ movies-api/utils/schemas/users.js
@@ -3,8 +3,14 @@ const joi = require('@hapi/joi');
 const userIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
 
 const createUserSchema = {
-  name: joi.string().max(100).required(),
-  email: joi.string().email().required(),
+  name: joi
+    .string()
+    .max(100)
+    .required(),
+  email: joi
+    .string()
+    .email()
+    .required(),
   password: joi.string().required(),
   isAdmin: joi.boolean()
 };
@@ -12,4 +18,4 @@ const createUserSchema = {
 module.exports = {
   userIdSchema,
   createUserSchema
-}
+};
